75f66243520338f3a23223583e49b9a9f76091b7
Antoniocns <acnspeixe@gmail.com>
2021-09-18 12:48:19 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+           string sobrenome;
+           string nomeCompleto;
+           string nomeCatalogo;
+                      
+           Console.Write("Digite seu primeiro nome: ");
+           nome = Console.ReadLine();
+           Console.Write("Digite seu sobrenome: ");
+           sobrenome= Console.ReadLine();
+           
+           Console.WriteLine($"Nome completo: {nome} {sobrenome.ToUpper()}");
+           Console.WriteLine($"Nome catalogo: {sobrenome.ToUpper()} {nome}");
+        }
+    }
+}
